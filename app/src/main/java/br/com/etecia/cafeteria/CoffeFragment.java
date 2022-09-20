5b831209ba28ce50cc313fbce96574a9d9ea6da8
ErickGX <ericksaopaulino1@hotmail.com>
2022-09-20 20:26:15 -0300

    msg

---
+++ app/src/main/java/br/com/etecia/cafeteria/CoffeFragment.java
@@ -0,0 +1,26 @@
+package br.com.etecia.cafeteria;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CoffeFragment extends Fragment {
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_coffe, container, false);
+        return view;
+    }
+}
